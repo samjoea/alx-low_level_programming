cb63317f9c3d020411d9a8d36f1f8ed000e6f1ed
Joseph Sam <ce-jsam4818@st.umat.edu.gh>
2022-06-08 01:54:12 +0000

    Task 0, _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - the main entry point
+ *
+ * Return: 0 when successful.
+ */
+
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar(10);
+
+	return (0);
+}
