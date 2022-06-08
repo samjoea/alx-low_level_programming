a9363ae7bb1c92e4ea61868e914b06333d7fff00
Joseph Sam <ce-jsam4818@st.umat.edu.gh>
2022-06-08 14:23:40 +0000

    Almost done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - takes two integers and adds them.
+ * @n: integer to add.
+ * @x: integer to add.
+ *
+ * Return: sum of n and x.
+ */
+
+int add(int n, int x)
+{
+	return (n + x);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Print from n to 98.
+ * @n: integer.
+ *
+ * Return: void.
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+
+	printf("%d\n", 98);
+}
