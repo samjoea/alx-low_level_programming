04b189b64d092948554776fddc5558a9513c7385
Joseph Sam <ce-jsam4818@st.umat.edu.gh>
2022-06-09 08:08:17 +0000

    Advance task 5

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - the main entry point
+ * Description: the program prints the first 50 fibonacci numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int i, j, k, sum;
+
+	sum = 2;
+	k = 1;
+	i = 2;
+	printf("%lu, ", k);
+	while (i < 98)
+	{
+		printf("%lu, ", sum);
+
+		j = sum;
+		sum += k;
+		k = j;
+
+		i++;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
