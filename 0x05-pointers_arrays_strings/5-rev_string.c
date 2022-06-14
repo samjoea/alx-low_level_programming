e8ae77217a0aeb40f854b4e626b9d477457503b3
Joseph Sam <ce-jsam4818@st.umat.edu.gh>
2022-06-14 02:19:18 +0000

    Task 5, reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - Prints a string in reverse
+ * @s: a pointer that points to a string
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, j, a;
+	char tmp;
+
+	for (i = 0; (*(s + i)) != '\0';)
+	{
+		i++;
+	}
+
+	j = i - 1;
+	a = 0;
+	while (a < j)
+	{
+		tmp = *(s + j);
+		*(s + j) = *(s + a);
+		*(s + a) = tmp;
+		
+		j--;
+		a++;
+	}	
+}
