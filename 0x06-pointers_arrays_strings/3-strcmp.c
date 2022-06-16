628a30bf0f487ccbf0b9f3d494e996935bacc4db
Joseph Sam <ce-jsam4818@st.umat.edu.gh>
2022-06-16 14:40:23 +0000

    task 3 strcmp fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; *(s1 + i) != '\0';)
-		i++;
-
-	for (j = 0; *(s2 + j) != '\0';)
-		j++;
-
-
-	if (i > j)
-		return ((i - j) * 10 + j);
-
-	else if (i < j)
-		return (-((j - i) * 10 + i));
+	for (i = 0; *(s1 + i) != '\0' || *(s2 + i); i++)
+	{
+		if (*(s1 + i) != *(s2 + i))
+			return (*(s1 + i) - *(s2 + i));
+	}
 
 	return (0);
 }
