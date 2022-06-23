f9c8fae06dd384953e48499cbbf480858dc57dcd
Joseph Sam <ce-jsam4818@st.umat.edu.gh>
2022-06-23 03:22:20 +0000

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,10 +9,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-
-	s++;
-	_print_rev_recursion(s);
-	_putchar(*s);
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*s);
+	}
 }
