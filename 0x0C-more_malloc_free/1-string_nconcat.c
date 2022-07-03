b2e556817f6d261e267df9d89031ae899190e8c1
Joseph Sam <ce-jsam4818@st.umat.edu.gh>
2022-07-03 12:40:55 +0000

    task 1, string concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,61 @@
 #include "main.h"
 
 /**
- * string_nconcat - a function that concatenates two strings.
- * @s1: first string
- * @s2: second string
- * @n: number of bytes
- * Return: a pointer char
+ * len - it calc the length of a string
+ * @s: pointer to string
+ *
+ * Return: an int value
+ */
+
+int len(char *s)
+{
+	if (*s != '\0')
+		return (1 + len(s + 1));
+	return (0);
+}
+
+/**
+ * string_nconcat - it concatenates two strings
+ * @s1: pointer to main string
+ * @s2: pointer to substring
+ * @n: n byte of s2 to be add s1
+ *
+ * Return: NULL is fail or pointer to the new string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int size1 = 0, size2 = 0, i;
+	unsigned int i, j, ln;
+	char *pstr;
 
-	if (s1 == NULL)
+	if (n > len(s2))
+		n = len(s2);
+
+	ln = len(s1);
+	pstr = malloc(sizeof *pstr * (ln + n + 1));
+	if (pstr == NULL)
+		return (NULL);
+	if (s1 == NULL && s2 == NULL)
+	{
 		s1 = "";
-
-	if (s2 == NULL)
+		s2 = "";
+	}
+	else if (s1 == NULL)
+		s1 = "";
+	else if (s2 == NULL)
 		s2 = "";
 
-	while (s1[size1] != '\0')
+	for (i = 0, j = 0; i < (n + ln); i++)
 	{
-		size1++;
+		if (i > ln - 1)
+		{
+			pstr[i] = s2[j];
+			j++;
+		}
+		else
+			pstr[i] = s1[i];
 	}
+	pstr[i] = '\0';
 
-	while (s2[size2] != '\0')
-	{
-		size2++;
-	}
-
-	if (n > size2)
-	n = size2;
-	p = malloc((size1 + n + 1) * sizeof(char));
-
-	if (p == NULL)
-		return (0);
-
-	for (i = 0; i < size1; i++)
-	{
-		p[i] = s1[i];
-	}
-
-	for (; i < (size1 + n); i++)
-	{
-		p[i] = s2[i - size1];
-	}
-	p[i] = '\0';
-
-return (p);
+	return (pstr);
 }
