e0a584125fe8557ad47983615d72980faccca60d
Joseph Sam <ce-jsam4818@st.umat.edu.gh>
2022-07-05 05:38:55 +0000

    function-like macro, sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File - Function like macro that calc the sum of two values;
+ * @x: int value
+ * @y: sencond value
+ */
+
+#define SUM(x, y) ((x) + (y))
+#endif
