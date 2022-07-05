56af238305c565228fdd7ac51699467320e29895
Joseph Sam <ce-jsam4818@st.umat.edu.gh>
2022-07-05 05:49:15 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type called dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
