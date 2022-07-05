7d940da887a1be45bf29165b8666d35843f34e7d
Joseph Sam <ce-jsam4818@st.umat.edu.gh>
2022-07-05 05:30:55 +0000

    function-like macro, abs

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File - Function like macro that calc the absolute value of x;
+ * @x: int value
+ */
+
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+#endif
