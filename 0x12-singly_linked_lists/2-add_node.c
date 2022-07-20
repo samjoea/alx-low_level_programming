991ac1e2a114eef9a3965c4527f9be6df9e479a1
Joseph Sam <ce-jsam4818@st.umat.edu.gh>
2022-07-20 07:38:33 +0000

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,18 +10,25 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	size_t i;
+	unsigned int i;
 	list_t *newl;
+	char *newstr;
 
 	newl = malloc(sizeof(list_t));
 	if (newl == NULL)
 		return (NULL);
 
-	newl->str = strdup(str);
-	for (i = 0; str;)
+	newstr = strdup(str);
+	if (newstr == NULL)
+	{
+		free(newl);
+		return (NULL);
+	}
+	newl->str = newstr;
+	for (i = 0; newstr;)
 		i++;
 
-	newl->len = i;
+	newl->len = i + 1;
 	newl->next = *head;
 	*head = newl;
 
